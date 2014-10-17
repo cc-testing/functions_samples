cbb15d285f8b0391a510b3790f0321cdf719632c
unknown <achoque@ACHOQUEWS6.roving.com>
2014-10-16 19:30:34 -0600

    teacher answers

--- wb_functions.js
+++ wb_functions.js
@@ -14,19 +14,21 @@
 
 fast(100);
 
-*/
+SPEED WITH BIRTHDAY - MY VERSION
 
 function fast (speed,luckyday) {
 
     var myspeed = speed;
     var lucky = luckyday;
+    var discspeed = 5;
 
     if ( myspeed < 60 ){
         console.log("you are not speeding. Have a nice day");
     } else if ( myspeed > 80 ) {
         console.log("you too fast you get no discount");
     } else if ( myspeed > 60 && myspeed < 80 && lucky == 1 ) {
-        console.log("you were speeding but you get a 5 mile discount");
+        var newspeed = myspeed - discspeed;
+        console.log("you speed was " + myspeed + " but it is your birthday so you get a 5 mile discount." + " Your birthday speed is " + newspeed )
     } else if ( myspeed > 60 && myspeed < 80 && lucky == 0 ) {
         console.log("you were speeding but since it is not your birthday, no discount");
     }
@@ -35,3 +37,31 @@ function fast (speed,luckyday) {
 }
 
 fast(75,0);
+
+  TEACHERS SOLUTION
+
+// using three parameters so limit is not harcoded
+
+function isSpeeding(speed, isBirthday, limit){
+
+    if (!limit) {    //if there is no limit, assing it to 60
+        limit = 60;
+    }
+
+    if(isBirthday) {   // if is birthday, increase limit to 5
+        limit += 5;
+    }
+
+    if (speed <= limit) {
+        return 0;
+    } else if (speed > limit && speed <= (limit + 20)) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+isSpeeding (65);
+
+    */
+
